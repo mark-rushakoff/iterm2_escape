3706acc614ec766aafd2befc912f8cfc9ada5b9b
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-03-09 00:42:21 -0800

    Use SUPPRESS_ITERM2_ESCAPE_GEM environment variable

--- lib/iterm2_escape/writer.rb
+++ lib/iterm2_escape/writer.rb
@@ -2,6 +2,7 @@ module Iterm2Escape
   module Writer
     class << self
       def write(message)
+        return unless ENV['SUPPRESS_ITERM2_ESCAPE_GEM'].nil? || ENV['SUPPRESS_ITERM2_ESCAPE_GEM'].empty?
         return unless ENV['TERM_PROGRAM'] == 'iTerm.app'
         $stdout.write(message)
       end
--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -1,15 +1,29 @@
 require 'spec_helper'
 
 describe Iterm2Escape::Writer do
-  it 'writes when the environment variables look like iTerm2' do
-    ENV.stub(:[]).with('TERM_PROGRAM').and_return('iTerm.app')
-    $stdout.should_receive(:write).with('Hi')
-    Iterm2Escape::Writer.write('Hi')
+  describe 'when suppressed by the environment' do
+    before { ENV.stub(:[]).with('SUPPRESS_ITERM2_ESCAPE_GEM').and_return('1') }
+
+    it "does not write" do
+      ENV.stub(:[]).with('TERM_PROGRAM').and_return('iTerm.app')
+      $stdout.should_not_receive(:write)
+      Iterm2Escape::Writer.write('Hi')
+    end
   end
 
-  it 'silently does not write when the environment variables do not look like iTerm2' do
-    ENV.stub(:[]).with('TERM_PROGRAM').and_return('Apple_Terminal')
-    $stdout.should_not_receive(:write)
-    Iterm2Escape::Writer.write('Hi')
+  describe 'when not suppressed by the environment' do
+    before { ENV.stub(:[]).with('SUPPRESS_ITERM2_ESCAPE_GEM').and_return(nil) }
+
+    it 'writes when the environment variables look like iTerm2' do
+      ENV.stub(:[]).with('TERM_PROGRAM').and_return('iTerm.app')
+      $stdout.should_receive(:write).with('Hi')
+      Iterm2Escape::Writer.write('Hi')
+    end
+
+    it 'silently does not write when the environment variables do not look like iTerm2' do
+      ENV.stub(:[]).with('TERM_PROGRAM').and_return('Apple_Terminal')
+      $stdout.should_not_receive(:write)
+      Iterm2Escape::Writer.write('Hi')
+    end
   end
 end
