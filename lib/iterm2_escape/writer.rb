d1b6b15c521fe42d7e041f17c21ca014f3c20115
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-03-09 00:55:44 -0800

    Extract should_write? method

--- lib/iterm2_escape/writer.rb
+++ lib/iterm2_escape/writer.rb
@@ -2,9 +2,13 @@ module Iterm2Escape
   module Writer
     class << self
       def write(message)
-        return unless ENV['SUPPRESS_ITERM2_ESCAPE_GEM'].nil? || ENV['SUPPRESS_ITERM2_ESCAPE_GEM'].empty?
-        return unless ENV['TERM_PROGRAM'] == 'iTerm.app'
+        return unless should_write?
         $stdout.write(message)
+      end
+
+      def should_write?
+        (ENV['SUPPRESS_ITERM2_ESCAPE_GEM'].nil? || ENV['SUPPRESS_ITERM2_ESCAPE_GEM'].empty?) &&
+          ENV['TERM_PROGRAM'] == 'iTerm.app'
       end
     end
   end
--- spec/writer_spec.rb
+++ spec/writer_spec.rb
@@ -1,29 +1,55 @@
 require 'spec_helper'
 
 describe Iterm2Escape::Writer do
-  describe 'when suppressed by the environment' do
-    before { ENV.stub(:[]).with('SUPPRESS_ITERM2_ESCAPE_GEM').and_return('1') }
+  describe '.should_write?' do
+    let(:subject) { Iterm2Escape::Writer.should_write? }
+    describe 'when $SUPPRESS_ITERM2_ESCAPE_GEM is set' do
+      before { ENV.stub(:[]).with('SUPPRESS_ITERM2_ESCAPE_GEM').and_return('1') }
 
-    it "does not write" do
-      ENV.stub(:[]).with('TERM_PROGRAM').and_return('iTerm.app')
-      $stdout.should_not_receive(:write)
-      Iterm2Escape::Writer.write('Hi')
+      describe 'and it looks like we are using iTerm2' do
+        before { ENV.stub(:[]).with('TERM_PROGRAM').and_return('iTerm.app') }
+        it "will not write" do
+          expect(subject).to eq(false)
+        end
+      end
+    end
+
+    describe 'when $SUPPRESS_ITERM2_ESCAPE_GEM is not set' do
+      before { ENV.stub(:[]).with('SUPPRESS_ITERM2_ESCAPE_GEM').and_return(nil) }
+
+      describe 'when it looks like we are in iTerm2' do
+        before { ENV.stub(:[]).with('TERM_PROGRAM').and_return('iTerm.app') }
+
+        it "will write" do
+          expect(subject).to eq(true)
+        end
+      end
+
+      describe 'when it does not look like we are in iTerm2' do
+        before { ENV.stub(:[]).with('TERM_PROGRAM').and_return('Apple_Terminal') }
+
+        it "will not write" do
+          expect(subject).to eq(false)
+        end
+      end
     end
   end
 
-  describe 'when not suppressed by the environment' do
-    before { ENV.stub(:[]).with('SUPPRESS_ITERM2_ESCAPE_GEM').and_return(nil) }
-
-    it 'writes when the environment variables look like iTerm2' do
-      ENV.stub(:[]).with('TERM_PROGRAM').and_return('iTerm.app')
-      $stdout.should_receive(:write).with('Hi')
-      Iterm2Escape::Writer.write('Hi')
+  describe '.write' do
+    describe 'when .should_write? is true' do
+      before { Iterm2Escape::Writer.stub(:should_write?).and_return(true) }
+      it 'writes' do
+        $stdout.should_receive(:write).with('Hi')
+        Iterm2Escape::Writer.write('Hi')
+      end
     end
 
-    it 'silently does not write when the environment variables do not look like iTerm2' do
-      ENV.stub(:[]).with('TERM_PROGRAM').and_return('Apple_Terminal')
-      $stdout.should_not_receive(:write)
-      Iterm2Escape::Writer.write('Hi')
+    describe 'when .should_write? is false' do
+      before { Iterm2Escape::Writer.stub(:should_write?).and_return(false) }
+      it 'does not write' do
+        $stdout.should_not_receive(:write)
+        Iterm2Escape::Writer.write('Hi')
+      end
     end
   end
 end
