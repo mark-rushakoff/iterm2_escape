325883955c895442ed4257c29eb824d35987aab8
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-03-10 23:38:53 -0700

    Clarify intent in set_tab_color example

--- examples/set_tab_color.rb
+++ examples/set_tab_color.rb
@@ -2,4 +2,5 @@ require 'iterm2_escape'
 
 abort "Usage: #{$0} RED GREEN BLUE" unless ARGV.size == 3
 
-Iterm2Escape::Tab.set_color(*(ARGV.map(&:to_i)))
+red, green, blue = *ARGV.map(&:to_i)
+Iterm2Escape::Tab.set_color(red, green, blue)
